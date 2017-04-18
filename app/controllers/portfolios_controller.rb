77f3d4e860f0c3fdf85669500e104a654893db90
Elise Adams <neuman.elise@gmail.com>
2017-04-18 12:03:30 -0600

    Impletmented custom scopes for portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,6 +4,10 @@ class PortfoliosController < ApplicationController
     @portfolio_items = Portfolio.all
   end
 
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+end
+
   def new 
     @portfolio_item = Portfolio.new
   end
@@ -12,7 +16,7 @@ def create
     @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle, :body))
 
     respond_to do |format|
-      if @portfolio_item.save
+      if @portfolio_item.save 
         format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
