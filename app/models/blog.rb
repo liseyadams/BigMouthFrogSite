0ca738059c8b6fcd71a1a34ab521d0fdbcd16203
Elise Adams <neuman.elise@gmail.com>
2017-05-12 10:14:39 -0600

    Updated the slug changes when updating a blog.

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -3,6 +3,10 @@ class Blog < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
 
+  def should_generate_new_friendly_id?
+  title_changed?
+  end
+
   validates_presence_of :title, :body, :topic_id
 
   belongs_to :topic
