6aa73f542b9f5b3ca5505e8855536aa38a89dcaa
Elise Adams <neuman.elise@gmail.com>
2017-06-03 08:41:58 -0600

    Integrated production calls for actioncable.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_cable.allowed_request_origins = ['https://eliseadamsportfolio.herokuapp.com', 'http://eliseadamsportfolio.herokuapp.com']
+  config.action_cable.url = "wss://eliseadamsportfolio.herokuapp.com/cable"
 end
